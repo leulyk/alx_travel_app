469ab6b1f0231ec9a0d1b2692f955e8dc1166f6f
Leul Yiheyis <leulykpro@gmail.com>
2025-05-18 23:26:15 +0300

    chore: initial configuration and setup

---
+++ alx_travel_app/settings.py
@@ -0,0 +1,31 @@
+import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
+
+INSTALLED_APPS = [
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+    'listings',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True  # For development only
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='3306'),
+    }
+}
